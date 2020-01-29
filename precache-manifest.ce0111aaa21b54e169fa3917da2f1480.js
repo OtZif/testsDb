f6780e540a2b1f4983dd64c637da969b299a3a9e
Zarif <zarif.otambekov@saltpepper.ru>
2020-01-29 17:10:38 +0300

    -

---
+++ precache-manifest.ce0111aaa21b54e169fa3917da2f1480.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f195ddf6e0c6267a28827da7ad362b6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78d893b0dc7dd3b546fb",
+    "url": "/static/css/main.5ea065ab.chunk.css"
+  },
+  {
+    "revision": "f5434df7952a61bc7b23",
+    "url": "/static/js/2.c6ad1b3c.chunk.js"
+  },
+  {
+    "revision": "78d893b0dc7dd3b546fb",
+    "url": "/static/js/main.bab02e7c.chunk.js"
+  },
+  {
+    "revision": "fe06b40b8a15b0342490",
+    "url": "/static/js/runtime-main.6b3d0059.js"
+  },
+  {
+    "revision": "0d6113073aa48f70ad3fb32ecd5a9ae9",
+    "url": "/static/media/shark.0d611307.png"
+  }
+]);
